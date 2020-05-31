81b4daa87fb2e01ba43d2f7611fb1b7605c93e84
Lê Đức Anh <ducanhgx@gmail.com>
2020-05-31 12:06:34 +0700

    build modules auth,user - swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import { UserModule } from './modules/user/user.module';
+// import { ArticleModule } from './article/article.module';
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { warn } from 'console';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // disableErrorMessages: true,
+    }),
+  );
+
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .addBearerAuth()
+    .setBasePath('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [
+      UserModule,
+      // ArticleModule,
+    ],
+  });
+  SwaggerModule.setup('api/docs', app, document);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  warn(`APP IS LISTENING TO PORT ${PORT}`);
 }
 bootstrap();
